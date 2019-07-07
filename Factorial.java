3bcabeec4bd62825b113fc803b52448045435182
Leonid Syrtsov <33368279+userdyadya@users.noreply.github.com>
2019-07-08 02:06:01 +0300

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,10 +1,11 @@
 public class Factorial {
-    public static void main(String[] args) {
-        int[] arrays = new int[11];                             // created arrays
+    public static void main(String[] args){
+        int[] arrays = new int[10];                             
         int sum = 0;
-        for (int i = 0; i < arrays.length; i++) {               // appropriated value to each elements
-            arrays[i] = i + 10;
-            sum = arrays[i] + sum;
+        Random random = new Random();
+        for (int i = 0; i < arrays.length; i++) {              
+            arrays[i] = random.nextInt(11) + 10;
+            sum += arrays[i];
         }
         System.out.println(sum / arrays.length);
     }
