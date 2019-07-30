e14073d7b26abb081b229a7b7574f3f1a86fffc4
Leonid Syrtsov <33368279+userdyadya@users.noreply.github.com>
2019-07-30 16:20:06 +0300

    Update StringBuilder.java

--- StringBuilder.java
+++ StringBuilder.java
@@ -1,8 +1,9 @@
 public class StringBuilder {
     public static void main(String[] args) {
         for (int i = 1; i <= 30; i++) {
-            String string = ("(" + i + ")");
-            System.out.print(string);                     //Сделал не правильно, переделаю.
+            StringBuilder result = new StringBuilder();
+            result.append("(").append(i).append(")");
+            System.out.print(result.toString());                     
         }
     }
 }
